2f5f585477ca1b8985a745e06bce528b2de249a4
paulholtom <59119271+paulholtom@users.noreply.github.com>
2025-10-05 08:01:32 -0600

    basic vue functioning

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+	<button @click="count++">Increase count</button>
+	<p>{{ count }}</p>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
